e7f8e6d7f1f879d5b619022192163a178f3f7261
erikbernard <erikbrsilva@gmail.com>
2025-08-28 00:42:24 -0300

    feat(DailyExchangeList): apply the pipe previous close, and implement unit testing

--- src/app/components/daily-exchange-list/daily-exchange-list.component.ts
+++ src/app/components/daily-exchange-list/daily-exchange-list.component.ts
@@ -1,15 +1,16 @@
-import {Component, inject, Input} from '@angular/core';
-import {DailyExchangeRateData} from "../../models/ExchangeRate.model";
-import {DailyExchangeItemComponent} from "../daily-exchange-item/daily-exchange-item.component";
-import {CommonModule} from "@angular/common";
-import {DomSanitizer, SafeHtml} from "@angular/platform-browser";
-import {MINUS, PLUS} from "../constants";
+import { Component, inject, Input } from '@angular/core';
+import { DailyExchangeRateData } from '../../models/ExchangeRate.model';
+import { DailyExchangeItemComponent } from '../daily-exchange-item/daily-exchange-item.component';
+import { CommonModule } from '@angular/common';
+import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
+import { PreviousClosePipe } from '../../pipes/previous-close.pipe';
+import { MINUS, PLUS } from '../constants';
 import { TOGGGLE } from '../animations';
 
 @Component({
   selector: 'app-daily-exchange-list',
   standalone: true,
-  imports: [CommonModule, DailyExchangeItemComponent],
+  imports: [CommonModule, DailyExchangeItemComponent, PreviousClosePipe],
   templateUrl: './daily-exchange-list.component.html',
   styleUrl: './daily-exchange-list.component.scss',
   animations: [TOGGGLE],
@@ -18,24 +19,16 @@ export class DailyExchangeListComponent {
   private readonly sanitizer = inject(DomSanitizer);
 
   @Input() history: DailyExchangeRateData[] = [];
-  isOpen: boolean = true;
-  // TODO: ajustar
+  isOpen: boolean = false;
 
   toggle(): void {
     this.isOpen = !this.isOpen;
   }
 
-  getPreviousClose(index: number): number {
-    if (index + 1 < this.history.length) {
-      return this.history[index + 1].close;
-    }
-    return index;
-  }
-
   get plusIcon(): SafeHtml {
-    return this.sanitizer.bypassSecurityTrustHtml(PLUS)
+    return this.sanitizer.bypassSecurityTrustHtml(PLUS);
   }
   get minusIcon(): SafeHtml {
-    return this.sanitizer.bypassSecurityTrustHtml(MINUS)
+    return this.sanitizer.bypassSecurityTrustHtml(MINUS);
   }
 }
