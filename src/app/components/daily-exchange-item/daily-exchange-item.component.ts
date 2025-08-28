1a162ffc5e810f92dafeb23dea5c9c94f50e71dd
erikbernard <erikbrsilva@gmail.com>
2025-08-28 00:40:51 -0300

    feat(DailyExchangeItem): apply the pipe calculate Diff, and implement unit testing

--- src/app/components/daily-exchange-item/daily-exchange-item.component.ts
+++ src/app/components/daily-exchange-item/daily-exchange-item.component.ts
@@ -8,29 +8,21 @@ import { DailyExchangeRateData } from '../../models/ExchangeRate.model';
 import { CommonModule } from '@angular/common';
 import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
 import { ARROW_DOWN, ARROW_UP } from '../constants';
+import { CalculateDiffPipe } from '../../pipes/calculate-diff.pipe';
 
 @Component({
   selector: 'app-daily-exchange-item',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, CalculateDiffPipe],
   templateUrl: './daily-exchange-item.component.html',
   styleUrl: './daily-exchange-item.component.scss',
-  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class DailyExchangeItemComponent {
   private readonly sanitizer = inject(DomSanitizer);
 
-  @Input() item: DailyExchangeRateData | null = null;
+  @Input() item!: DailyExchangeRateData;
   @Input() previousClose: number = 0;
 
-  get diffPercentage(): number | null {
-    if (this.item && this.previousClose) {
-      return (
-        (this.item.close - this.previousClose) * 100
-      );
-    }
-    return (this.item!.close / this.previousClose) * 100;
-  }
   get arrowUp(): SafeHtml {
     return this.sanitizer.bypassSecurityTrustHtml(ARROW_UP);
   }
